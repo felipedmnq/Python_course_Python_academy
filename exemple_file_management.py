7c1975ea4c1b168ebee92f32f23090994b36bcc6
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-10-02 06:32:03 -0300

    advanced modules - file management

--- exemple_file_management.py
+++ exemple_file_management.py
@@ -35,3 +35,42 @@ for line in file:
     line = line.replace('\n', '') # --> funciona como o (end='')
     #print(line, end='')
     print(line)
+
+print('-' * 30)
+# ANEXAR DADOS AO ARQUIVO
+# "w" escreve no arquivo apagando o texto antigo.
+file = open('teste.txt', 'w')
+file.write('Ola Felipe')        # write para estrever no arquivo.txt
+file.write('\nesse texto aparece e substitui o anterior quando "W" eh o modo.')
+# "\n" quebra linha.
+
+# para ler o arquivo novamente.
+file = open('teste.txt', 'r')
+print(file.read())
+
+# "a" == .append --> vai anexando a informação ao final do texto
+# a cada execução.
+file = open('teste.txt', 'a')
+file.write('\nesse texto apare e eh repitido quando "A" é o modo.')
+# cada vez que eu executo o comando, ele escreve novamente no arquivo.
+# fica salvo.
+
+# quando o modo é "W" (write), deve-se sempre "fechar" o arquivo.
+# regra geral - quando se abre um arquivo para escrita, o sistema bloqueia
+# o acesso a outros usuários para escrita, por isso deve-se fechar o arquivo.
+
+#FECHAR ARQUIVO.
+file.close()  # encerra o objeto file.
+
+# abrir arquivo para "create" -- "X".
+file = open('new_file.txt', 'x')  # novo arquivo criado
+file.write('Novo arquivo criado')
+file.close()
+
+# ler o novo arquivo.
+file = open('new_file.txt', 'r')
+print(file.read())
+
+
+
+
